dcf90366d545301f259b3eb17af0cc694d441cd6
Jonatan Gomez <111893192+Joangopa@users.noreply.github.com>
2025-06-26 23:37:28 -0300

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -35,7 +35,6 @@ with aba1:
     col1, col2, col3 = st.columns([1, 2, 1])  # Centralizar a imagem
     with col2:
         st.image("assets/imagem_titulo.png", 
-                 caption="Ilustração do problema",
                  width=400)
 
 # --- Aba 2: Texto e imagem com tipos celulares ---
@@ -45,80 +44,95 @@ with aba2:
     col1, col2 = st.columns([3, 2])
 
     with col1:
-        st.write("""
+        st.write("""<div style='text-align: justify; font-size: 18px;'>
             Este estudo analisa como diferentes ambientes moldam a imunidade materna durante a gravidez, 
-            comparando populações expostas a níveis contrastantes de microrganismos.
-        """)
+            comparando populações expostas a níveis contrastantes de microrganismos. 
+        </div>""", unsafe_allow_html=True)
 
-        st.write("""
-            👩🏽‍🦱 Mulheres Tsimane, de uma comunidade indígena na floresta boliviana, com alta exposição a patógenos naturais e fertilidade tradicional.
-
-            👩🏼 Mulheres nos EUA, vivendo em ambiente urbano com baixa exposição microbiana.
-
+        st.write(""" <div style='text-align: justify; font-size: 18px;'> <br>
+            👩🏽‍🦱 Mulheres Tsimane, de uma comunidade indígena na floresta boliviana, com alta exposição a patógenos naturais e fertilidade tradicional. <br>
+            👩🏼 Mulheres nos EUA, vivendo em ambiente urbano com baixa exposição microbiana. <br> <br>
             O foco está na análise de seis tipos de células imunológicas:
-        """)
+        </div>""", unsafe_allow_html=True)
+
 
         col_esq, col_dir = st.columns(2)
+        
         with col_esq:
-            st.markdown("- **Leucócitos totais**")
-            st.markdown("- **Linfócitos**")
-            st.markdown("- **Neutrófilos**")
+            st.markdown("<div style='font-size: 18px;'>- <b>Leucócitos totais</b></div>", unsafe_allow_html=True)
+            st.markdown("<div style='font-size: 18px;'>- <b>Linfócitos</b></div>", unsafe_allow_html=True)
+            st.markdown("<div style='font-size: 18px;'>- <b>Neutrófilos</b></div>", unsafe_allow_html=True)
 
         with col_dir:
-            st.markdown("- **Monócitos**")
-            st.markdown("- **Eosinófilos**")
-            st.markdown("- **Basófilos**")
+            st.markdown("<div style='font-size: 18px;'>- <b>Monócitos</b></div>", unsafe_allow_html=True)
+            st.markdown("<div style='font-size: 18px;'>- <b>Eosinófilos</b></div>", unsafe_allow_html=True)
+            st.markdown("<div style='font-size: 18px;'>- <b>Basófilos</b></div>", unsafe_allow_html=True)
 
     with col2:
         st.image("assets/imagem_tipo_celulas.png", 
                  caption="Tipos de Células Imunológicas e função",
-                 use_container_width=True)
+                 width=300)
 
 
 # --- Aba 3 ---
 with aba3:
     st.header("📊 Dados e Variáveis")
 
-    st.write("""
-    O conjunto de dados considera dois tipos de populações. A primeira delas corresponde a dados de mulheres Tsimane.  
-    A segunda fonte de dados corresponde a dados de mulheres morando nos Estados Unidos.  
-    A idade das mulheres das duas fontes de dados varia entre 18 e 45 anos.
-    """)
-    
-    st.write("Consideramos 6 grupos de variáveis no conjunto de dados distribuídos da seguinte forma:")
-
     col1, col2 = st.columns([3, 2])
 
     with col1:
-        st.subheader("Descrição das Variáveis")
 
-        st.markdown("**Contagem Leucocitária (células/µL)**")
-        st.markdown("- WBC: Leucócitos totais")
-        st.markdown("- NEU: Neutrófilos")
-        st.markdown("- LYM: Linfócitos")
-        st.markdown("- MON: Monócitos")
-        st.markdown("- EOS: Eosinófilos")
-        st.markdown("- BAS: Basófilos")
+        st.markdown("""
+        <div style='font-size: 18px; text-align: justify;'>
+        O conjunto de dados considera dois tipos de populações. A primeira delas corresponde a dados de mulheres Tsimane.  
+         A segunda fonte de dados corresponde a dados de mulheres morando nos Estados Unidos.  
+        <br> A idade das mulheres das duas fontes de dados varia entre 18 e 45 anos.
+        </div>
+        """, unsafe_allow_html=True)
+        
+        st.markdown("<div style='font-size: 18px;'> <br> Consideramos 6 grupos de variáveis no conjunto de dados distribuídos da seguinte forma:</div>", unsafe_allow_html=True)
 
-        st.markdown("**Dados Antropométricos**")
-        st.markdown("- BMI: Índice de massa corporal (kg/m²)")
-        st.markdown("- Age: Idade em anos")
+         
+        col1_1, col1_2 = st.columns([2,3])
+        with col1_1:
+        
+            st.markdown("<div style='font-size: 18px;'>", unsafe_allow_html=True)
 
-        st.markdown("**Dados Reprodutivos**")
-        st.markdown("- NumPartos: Número de partos")
-        st.markdown("- RepStatus: Status reprodutivo (Cycling: Não grávida. T1, T2, T3: Trimestre da gravidez)")
+            st.markdown("**Contagem Leucocitária (células/µL)**", unsafe_allow_html=True)
+            st.markdown("- WBC: Leucócitos totais", unsafe_allow_html=True)
+            st.markdown("- NEU: Neutrófilos", unsafe_allow_html=True)
+            st.markdown("- LYM: Linfócitos", unsafe_allow_html=True)
+            st.markdown("- MON: Monócitos", unsafe_allow_html=True)
+            st.markdown("- EOS: Eosinófilos", unsafe_allow_html=True)
+            st.markdown("- BAS: Basófilos", unsafe_allow_html=True)
 
-        st.markdown("**Metadados**")
-        st.markdown("- Population: Origem populacional (THLHP: Tsimane, NHANES: Mulheres EUA)")
+            st.markdown("</div>", unsafe_allow_html=True)
+
+        with col1_2:
+
+            st.markdown("<div style='font-size: 18px;'>", unsafe_allow_html=True)
+
+            st.markdown("**Dados Antropométricos**", unsafe_allow_html=True)
+            st.markdown("- BMI: Índice de massa corporal (kg/m²)", unsafe_allow_html=True)
+            st.markdown("- Age: Idade em anos", unsafe_allow_html=True)
+
+            st.markdown("**Dados Reprodutivos**", unsafe_allow_html=True)
+            st.markdown("- NumPartos: Número de partos", unsafe_allow_html=True)
+            st.markdown("- RepStatus: Estado reprodutivo (Cycling: Não grávida. T1, T2, T3: Trimestre da gravidez)", unsafe_allow_html=True)
+
+            st.markdown("**Metadados**", unsafe_allow_html=True)
+            st.markdown("- Population: Origem populacional (THLHP: Tsimane, NHANES: Mulheres EUA)", unsafe_allow_html=True)
+
+            st.markdown("</div>", unsafe_allow_html=True)
 
     with col2:
-        st.subheader("Distribuição dos Dados")
+        st.subheader("Distribuição dos Dados por Estado Reprodutivo e Origem Populacional")
 
         try:
             image = Image.open("assets/imagem_relacao_gravidas_dados_finais.png")
             st.image(image, 
                      caption="Distribuição por estado reprodutivo e origem populacional",
-                     use_container_width=True)
+                     width=350)
         except FileNotFoundError:
             st.warning("Imagem não encontrada no caminho especificado")
         except Exception as e:
@@ -147,7 +161,18 @@ with aba4:
 
     # --- HISTOGRAMAS ---
     with col1:
-        st.subheader("Distribuição das Contagens Celulares na População Geral")
+
+        # Dicionário de nomes descritivos
+        cell_type_names = {
+            'WBC': 'Leucócitos totais',
+            'NEU': 'Neutrófilos',
+            'LYM': 'Linfócitos',
+            'MON': 'Monócitos',
+            'EOS': 'Eosinófilos',
+            'BAS': 'Basófilos'
+        }
+
+        st.subheader("Distribuição das Contagens Leucocitárias na População Geral")
         variaveis = ['WBC', 'NEU', 'LYM', 'MON', 'BAS', 'EOS']
         fig1, axes = plt.subplots(2, 3, figsize=(12, 8))
         sns.set(style="whitegrid")
@@ -155,16 +180,52 @@ with aba4:
 
         for ax, var in zip(axes.flat, variaveis):
             sns.histplot(data=dados, x=var, kde=True, bins=30, color='skyblue', ax=ax)
-            ax.set_title(f'Distribuição de {var}', fontsize=14)
-            ax.set_xlabel(f'{var} (cells/μL)', fontsize=12)
+            ax.set_title(f'Distribuição da contagem de {cell_type_names[var]}', fontsize=14)
+            ax.set_xlabel(f'{cell_type_names[var]} (cells/μL)', fontsize=12)
             ax.set_ylabel('Frequência', fontsize=12)
 
         plt.tight_layout()
         st.pyplot(fig1)
 
-    # --- GRÁFICO VIOLINO ---
+    
     with col2:
-        st.subheader("Comparação por Status Reprodutivo e População")
+        st.subheader("K-means e PCA: Separação Populacional")
+        st.image("assets/agrupamentos_pca.png", 
+                 caption="Agrupamentos K-means com projeção PCA",
+                 width=500)
+        
+        
+
+        
+
+    # --- IMAGENS FINAIS ---
+    col3, col4 = st.columns(2)
+
+    # --- GRÁFICO VIOLINO ---
+    with col3:
+
+        # Dicionário de nomes descritivos
+        cell_type_names = {
+            'WBC': 'Leucócitos totais',
+            'NEU': 'Neutrófilos',
+            'LYM': 'Linfócitos',
+            'MON': 'Monócitos',
+            'EOS': 'Eosinófilos',
+            'BAS': 'Basófilos'
+        }
+
+        st.subheader("Comparação por Estado Reprodutivo e População")
+
+        st.markdown(
+            """
+            <div style='text-align: justify; font-size: 15px;'>
+            🔍 Contagens das células leucocitárias entre diferentes estados reprodutivos (Cycling, T1, T2, T3) e populações (NHANES e THLHP).
+            As violas representam a distribuição dos dados, enquanto os círculos pretos indicam a média para cada grupo.
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+
         cell_types = ['WBC', 'NEU', 'LYM', 'MON', 'BAS', 'EOS']
         dados_long = dados.melt(
             id_vars=['RepStatus', 'Population'],
@@ -209,8 +270,8 @@ with aba4:
                     ax.scatter(j + offset, mean_val, color='black', s=50, marker='o', zorder=10,
                                edgecolor='white', linewidth=0.8)
 
-            ax.set_title(cell_type, fontsize=14)
-            ax.set_xlabel("Status Reprodutivo", fontsize=12)
+            ax.set_title(cell_type_names[cell_type], fontsize=14)
+            ax.set_xlabel("Estado Reprodutivo", fontsize=12)
             ax.set_ylabel("cells/μL", fontsize=12)
 
             if i == 0:
@@ -222,24 +283,20 @@ with aba4:
         plt.tight_layout()
         st.pyplot(fig2)
 
-    # --- IMAGENS FINAIS ---
-    col3, col4 = st.columns(2)
-
-    with col3:
-        st.subheader("Resultados do teste U: Grupos com Menores Contagens")
-        st.image("assets/imagem_tabela_menor_contagem.png", 
-                 caption="População com menor contagem média para cada tipo celular",
-                 width=500)
+        
 
     with col4:
-        st.subheader("K-means e PCA: Separação Populacional")
-        st.image("assets/agrupamentos_pca.png", 
-                 caption="Agrupamentos K-means com projeção PCA",
+
+        st.subheader("Resultados do teste U: Grupos com Menores Contagens")
+        st.image("assets/imagem_tabela_menor_contagem.png", 
+                 caption="População com menor contagem média para cada tipo celular e estado reprodutivo",
                  width=500)
+        
+        
         
 # --- Aba 5: modelo ML ---
 with aba5:
-    st.header("🤖 Modelo de Aprendizado de Máquina")
+    st.header("Modelo de Aprendizado de Máquina")
 
     modelo = load("model/modelo_gradient_boosting.pkl")
     scaler = load("model/scaler.pkl")
@@ -247,33 +304,70 @@ with aba5:
     col1, col2 = st.columns(2)
     with col1:
         st.write("""
+        <div style='text-align: justify; font-size: 18px;'>
         A escolha de um modelo de regressão para estimar os níveis de neutrófilos (NEU) resulta ser estratégica por utilizar variáveis acessíveis e de baixo custo: 
         contagem de leucócitos (WBC), IMC, idade, histórico reprodutivo e grupo populacional.
-        """)
+        </div>
+        """, unsafe_allow_html=True)
+        
         st.write("""
+        <div style='text-align: justify; font-size: 18px;'>
         O modelo escolhido a partir de metricas como MAE e R²-ajustado, é o GradientBoosting Regressor.
-        """)
+        </div>
+        """, unsafe_allow_html=True)
 
         st.write("""
-        | Métrica       | Valor       |
+        <div style='text-align: justify; font-size: 18px;'>
+        A seguir, apresentamos as métricas de desempenho do modelo, que demonstram sua eficácia na previsão dos níveis de neutrófilos com base nas variáveis selecionadas.
+        </div>
+        """, unsafe_allow_html=True)
+        st.write("""                 
+                 | Métrica       | Valor       |
         |---------------|-------------|
         | MAE           | 616.030     |
         | R²            | 0.802       |
         | R²-ajustado   | 0.799       |
-        """)
+        """) 
+       
 
     with col2:
-        st.image("assets/matiz_correlacao_ml.png", caption="Matriz de Correlação para as Variáveis Preditoras do Modelo de Regressão", use_container_width=True)
-
+        st.image(
+        "assets/matiz_correlacao_ml.png",
+        caption="Matriz de Correlação para as Variáveis Preditoras do Modelo de Regressão",
+        width=500  # ajuste conforme necessário
+        )
     st.markdown("---")
     st.subheader("Desempenho do Modelo GradientBoosting Regressor")
 
     col3, col4 = st.columns(2)
-    with col3:
-        st.image("assets/impato_variaveis_modelo.png", caption="Importância das Variáveis no Modelo", use_container_width=True)
     with col4:
-        st.image("assets/real_vs_predict_regressao.png", caption="Valores Reais vs. Preditos pelo Modelo", use_container_width=True)
+        st.markdown(
+            """
+            <div style='text-align: justify; font-size: 18px;'>
+            O seguinte gráfico  apresenta a influência das varaiveis nas estimativas do modelo, destacando tanto a direção quanto a intensidade desse impacto.
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+        
+        st.image(
+        "assets/impato_variaveis_modelo.png",
+        caption="Impacto das variaveis na estimativa da contagem de neutrófilos",
+        width=450  # ajuste conforme necessário
+        )
+        
+    with col3:
 
+        st.write("""        <div style='text-align: justify; font-size: 18px;'>
+        <br> 
+                 </div>""", unsafe_allow_html=True)
+        
+        st.image(
+        "assets/real_vs_predict_regressao.png",
+        caption="Valores Reais vs. Preditos pelo Modelo",
+        width=400  # ajuste conforme necessário
+        )
+        
     #st.image("assets/impato_variaveis_modelo.png", caption="Importância das Variáveis no Modelo", width=700)
 
     st.markdown("---")
@@ -321,36 +415,43 @@ with aba6:
 
     st.header("📌 Considerações Finais")
 
-    st.markdown("""
-    **A partir do conjunto de dados utilizado neste trabalho, pode-se concluir que:**
-    """)
+    st.write(
+        """<div style='text-align: justify; font-size: 18px;'>
+        A partir do conjunto de dados e análises utilizado neste trabalho, pode-se concluir que:
+        </div>""",
+        unsafe_allow_html=True
+    )
 
-    # Lista de conclusões com marcadores
     conclusions = [
-        "As duas populações estudadas apresentam perfis distintos de contagem leucocitária, "
+        "1. As duas populações estudadas apresentam perfis distintos de contagem leucocitária, "
         "refletindo provavelmente diferenças em seus ambientes e condições de vida.",
-        
-        "O estado reprodutivo (especialmente a gravidez) demonstra influência significativa "
+
+        "2. O estado reprodutivo (especialmente a gravidez) demonstra influência significativa "
         "nos parâmetros imunológicos analisados.",
-        
-        "A abordagem de machine learning mostrou-se eficaz para modelar as complexas interações "
+
+        "3. A abordagem de machine learning mostrou-se eficaz para modelar as complexas interações "
         "entre variáveis biológicas, antropométricas e populacionais.",
-        
-        "A inclusão de metadados mais detalhados sobre a população americana poderia enriquecer "
+
+        "4. A inclusão de metadados mais detalhados sobre a população americana poderia enriquecer "
         "as análises comparativas."
     ]
 
+    
     for conclusion in conclusions:
-        st.markdown(f"- {conclusion}")
+        st.write(f"<div style='text-align: justify; font-size: 18px;'>{conclusion}</div>", unsafe_allow_html=True)
+    
 
     st.markdown("---")
 
     st.subheader("Recomendações para Pesquisas Futuras")
-    st.markdown("""
-    Sugere-se para trabalhos futuros:
-    
-    1. **Ampliação da amostra**: Incluir outras populações para análise comparativa  
-    2. **Dados adicionais**: Coletar informações sobre dieta, atividade física e exposição ambiental  
-    3. **Modelagem avançada**: Explorar técnicas de deep learning para padrões mais complexos  
-    4. **Estudo longitudinal**: Acompanhar as variações nos parâmetros ao longo do tempo  
-    """)
+
+    st.write(
+        """<div style='font-size: 18px;'>
+        Sugere-se para trabalhos futuros: <br>
+        1. <b>Ampliação da amostra</b>: Incluir outras populações para análise comparativa  <br>
+        2. <b>Dados adicionais</b>: Coletar informações sobre dieta, atividade física e exposição ambiental  <br>
+        3. <b>Modelagem avançada</b>: Explorar técnicas de deep learning para padrões mais complexos  <br>
+        4. <b>Estudo longitudinal</b>: Acompanhar as variações nos parâmetros ao longo do tempo  
+        </div>""",
+        unsafe_allow_html=True
+    )
